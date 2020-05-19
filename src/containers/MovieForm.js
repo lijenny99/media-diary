da6e6ef1137c76420ccbed108e1ee58a4074fbd1
Jenny Li <lijenny1999@gmail.com>
2020-05-19 19:58:58 -0400

    add submit

--- src/containers/MovieForm.js
+++ src/containers/MovieForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Input, Rate, Switch, Button } from 'antd';
+import { Form, Input, Rate, Switch, Button} from 'antd';
 import axios from 'axios';
 
 const { TextArea } = Input;
@@ -11,30 +11,63 @@ class MovieForm extends Component {
         title: '',
         director: '', 
         year: '',
+        posterImg: '',
+        posterShow: false,
+        rating: 0,
+        spoilers: false,
+        entry: ''
     }
     getMovieData = () => {
         axios.get('http://www.omdbapi.com/?t=' + this.state.title +'&apikey=3fa2007d')
             .then(response => {
                 this.setState({
                     director: response.data['Director'],
-                    year: response.data['Year']
+                    year: response.data['Year'],
+                    posterImg: response.data['Poster'],
+                    title: response.data['Title'],
+                    posterShow: true
                 })
             })
             .catch(error => {
                 console.log(error);
             });
-        axios.get('http://img.omdbapi.com/?apikey=3fa2007d&t='+this.state.title).then(response => {
-            console.log(response);
-        }).catch(error => {
-            console.log(error);
-        })
     }
 
-    handleChange = (event) => {
+    submitMovieData = () => {
+        const movData = {
+            title: this.state.title,
+            posterImg: this.state.posterImg,
+            rating: this.state.rating,
+            entry: this.state.entry
+        }
+        axios.post( 'https://media-diary-25762.firebaseio.com/movies.json', movData )
+            .then( response => {
+                alert('done!'+response);
+            } )
+            .catch( error => {
+                console.log(error);
+            } );
+    }
+
+    handleTitleChange = (event) => {
         this.setState({ title: event.target.value });
     }
 
+    // handleRatingChange = (event) => {
+    //     this.setState({ rating: event.target.value });
+    // }
+
+    handleEntryChange = (event) => {
+        this.setState({ entry: event.target.value });
+    }
+
     render() {
+        let poster = null
+        if (this.state.posterShow) {
+            poster = (
+                <img className="Poster" src={this.state.posterImg} alt="Movie poster"></img>
+            )
+        }
         return (
             <div className="MovieForm">
                 <Form>
@@ -42,7 +75,7 @@ class MovieForm extends Component {
                         label="Movie Title"
                         name="title"
                         value={this.state.title}
-                        onChange={this.handleChange}>
+                        onChange={this.handleTitleChange}>
                         <Input />
                     </Form.Item>
                 </Form>
@@ -58,24 +91,32 @@ class MovieForm extends Component {
 
 
                     <Form.Item name="rate" label="Rating">
-                        <Rate allowHalf tooltips={desc} />
+                        <Rate allowHalf tooltips={desc} 
+                        value={this.state.rating}
+                        onChange={this.handleRatingChange}/>
                     </Form.Item>
 
-                    <Form.Item name="switch" label="Spoilers" valuePropName="checked">
+                    <Form.Item name="switch" label="Spoilers?" valuePropName="checked" colon={false}>
                         <Switch />
                     </Form.Item>
                 </Form>
 
                 <Form>
-                    <TextArea placeholder="How did this movie make you feel 😌" autoSize />
+                    <TextArea placeholder="How did this movie make you feel 😌" autoSize={{ minRows: 3 }} 
+                    value={this.state.entry}
+                    onChange={this.handleEntryChange}/>
 
                     <Form.Item style={{ marginTop: '40px', marginBottom: '0px' }}>
                         <Button type="primary" htmlType="submit" onClick={this.getMovieData}>
-                            Submit
+                            Display Data
+                        </Button>
+
+                        <Button type="default" htmlType="submit" onClick={this.submitMovieData}>
+                            Upload Data
                         </Button>
                     </Form.Item>
-
                 </Form>
+                {poster}
             </div>
         )
     }
