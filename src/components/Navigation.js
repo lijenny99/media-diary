fdd24c7581e10533a457e428b141265cff12f48a
Jenny Li <lijenny1999@gmail.com>
2020-06-09 19:53:49 -0400

    fixed active link on refresh

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Menu } from 'antd';
 import { HomeOutlined, BookOutlined, PlaySquareOutlined, LoginOutlined } from '@ant-design/icons';
 
 const Navigation = (props) => {
+    const location = useLocation();
 
     return (
         <div>
-            <Menu mode="horizontal" className="Menu">
-                <Menu.Item key="home" icon={<HomeOutlined />}>
-                    <Link to="/">Home</Link></Menu.Item>
+            <Menu mode="horizontal" className="Menu" selectedKeys={[location.pathname]}>
+                <Menu.Item key="/" icon={<HomeOutlined />}>
+                    <NavLink to="/">Home</NavLink></Menu.Item>
                 {props.isAuthenticated ? [
-                    <Menu.Item key="book" icon={<BookOutlined />}>
-                        <Link to="/books">Books</Link></Menu.Item>,
-                    <Menu.Item key="movie" icon={<PlaySquareOutlined />}>
-                        <Link to="/movies">Movies</Link></Menu.Item>
+                    <Menu.Item key="/book" icon={<BookOutlined />}>
+                        <NavLink to="/books">Books</NavLink></Menu.Item>,
+                    <Menu.Item key="/movie" icon={<PlaySquareOutlined />}>
+                        <NavLink to="/movies">Movies</NavLink></Menu.Item>
                 ] : null}
-                <Menu.Item key="login" icon={<LoginOutlined />}>
-                    <Link to="/login">Login</Link></Menu.Item>
+                <Menu.Item key="/login" icon={<LoginOutlined />}>
+                    <NavLink to="/login">Login</NavLink></Menu.Item>
             </Menu>
         </div>
     )
