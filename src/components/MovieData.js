ae4abffc910d09abf0534d6d489cf7fd5733a372
Jenny Li <lijenny1999@gmail.com>
2020-06-16 18:12:42 -0400

    landing page styling

--- src/components/MovieData.js
+++ src/components/MovieData.js
@@ -1,43 +1,51 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'antd';
 
 function MovieData(props) {
 
-    const { Meta } = Card;
-    const [clicked, handleClick] = useState(false);
+    // const { Meta } = Card;
+    // const [clicked, handleClick] = useState(false);
 
-    let newTitle = props.title + " (" + props.rating + "/5)"
+    // let newTitle = props.title + " (" + props.rating + "/5)"
 
-    let buttonDisplay = null
-    if (props.spoilers && !clicked) {
-        buttonDisplay = (
-            <Button className="center-button" onClick={() => handleClick(true)}>View Spoilers</Button>
-        )
-    }
+    // let buttonDisplay = null
+    // if (props.spoilers && !clicked) {
+    //     buttonDisplay = (
+    //         <Button className="center-button" onClick={() => handleClick(true)}>View Spoilers</Button>
+    //     )
+    // }
 
     return (
-        <Card className="MovieCard"
-            cover={
-                <img style={{ height: '300px', width: 'auto' }} // make this more dynamic
-                    alt="example"
-                    src={props.poster}
-                />
-            }>
-            <Meta
-                title={newTitle}
-                description={props.synopsis}
-                style={{ marginBottom: '15px'}}
+        <div style={{display: 'inline-block'}}>
+            <img className="MoviePoster" // make this more dynamic
+                alt="Movie poster"
+                src={props.poster}
             />
-            <div style={{ position: 'relative' }}>
-                {buttonDisplay}
-                <p className={
-                    !props.spoilers ? null :
-                    [
-                        !clicked ? 'blurry-text' : null
-                    ]
-                }>{props.entry}</p>
-            </div>
-        </Card>
+            <p className="MovieTitle">{props.title}</p>
+        </div>
+
+        // <Card className="MovieCard"
+        //     cover={
+        //         <img style={{ height: '300px', width: 'auto' }} // make this more dynamic
+        //             alt="example"
+        //             src={props.poster}
+        //         />
+        //     }>
+        //     <Meta
+        //         title={newTitle}
+        //         description={props.synopsis}
+        //         style={{ marginBottom: '15px'}}
+        //     />
+        //     <div style={{ position: 'relative' }}>
+        //         {buttonDisplay}
+        //         <p className={
+        //             !props.spoilers ? null :
+        //             [
+        //                 !clicked ? 'blurry-text' : null
+        //             ]
+        //         }>{props.entry}</p>
+        //     </div>
+        // </Card>
     );
 };
 
